0da1995a9e25f99d438644677b5ea5b0a2ca744b
Dom Kiva-Meyer <git@domkm.com>
2013-04-15 20:29:17 -0700

    expectations complete

--- test/expectations_test.rb
+++ test/expectations_test.rb
@@ -1,2 +1,11 @@
 require 'test_helper'
 
+describe MiniTest::Expectations do
+  MiniTest::Expectations.instance_methods.each do |method|
+    next unless match = /^wont(.*)/.match(method)
+    it "aliases #{method} to must_not#{match[1]}" do
+      assert_equal MiniTest::Expectations.instance_method(method),
+                   MiniTest::Expectations.instance_method("must_not#{match[1]}")
+    end
+  end
+end
