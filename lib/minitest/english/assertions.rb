18c6cbf1ebd1367e81afd527a4fa3074f43492af
Dom Kiva-Meyer <git@domkm.com>
2013-04-15 20:10:24 -0700

    assertions complete

--- lib/minitest/english/assertions.rb
+++ lib/minitest/english/assertions.rb
@@ -1,1 +1,8 @@
 require 'minitest/unit'
+
+module MiniTest::Assertions
+	instance_methods.each do |method|
+		next unless match = /^refute(.*)/.match(method)
+		alias_method "deny#{match[1]}", method
+	end
+end
--- test/assertions_test.rb
+++ test/assertions_test.rb
@@ -1,1 +1,11 @@
 require 'test_helper'
+
+describe MiniTest::Assertions do
+  MiniTest::Assertions.instance_methods.each do |method|
+    next unless match = /^refute(.*)/.match(method)
+    it "aliases #{method} to deny#{match[1]}" do
+      assert_equal MiniTest::Assertions.instance_method(method),
+                   MiniTest::Assertions.instance_method("deny#{match[1]}")
+    end
+  end
+end
